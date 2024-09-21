728f94abec4c5851c5f73c9925264071e292ad9e
Pranavkangale <kangalepranav@gmail.com>
2024-09-21 23:21:48 +0530

    Lamding page done

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,12 +1,41 @@
+import GlassSheet from "@/components/global/glass-sheet"
+import { Button } from "@/components/ui/button"
+import { Logout } from "@/icons"
+import { MenuIcon } from "lucide-react"
+import Link from "next/link"
 import Menu from "./menu"
 
 type Props = {}
 
-const LandingPageNavbar = (props: MenuProps) => {
+const LandingPageNavbar = (props: Props) => {
     return (
         <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
             <p className="font-bold text-2xl">Veri5ed.</p>
             <Menu orientation="desktop" />
+            <div className="flex gap-2">
+                <Link href="/sign-in">
+                    <Button
+                        variant="outline"
+                        className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
+                    >
+                        <Logout />
+                        Login
+                    </Button>
+                </Link>
+                <GlassSheet
+                    triggerClass="lg:hidden"
+                    trigger={
+                        <Button
+                            variant="ghost"
+                            className="hover:bg-transparent"
+                        >
+                            <MenuIcon size={30} />
+                        </Button>
+                    }
+                >
+                    <Menu orientation="mobile" />
+                </GlassSheet>
+            </div>
         </div>
     )
 }
