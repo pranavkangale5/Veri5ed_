b8ea018e2ba4cd06dd03577fa57f0c2c584c689d
Pranavkangale <kangalepranav@gmail.com>
2024-09-27 17:39:59 +0530

    Update auth.ts

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,15 +1,25 @@
 "use server"
 
+import { client } from "@/lib/prisma"
 import { currentUser } from "@clerk/nextjs/server"
 
 export const onAuthenticatedUser = async () => {
     try {
         const clerk = await currentUser()
-        if (!clerk) {
+        if (!clerk)
             return {
                 status: 404,
             }
-        }
+        const user = await client.user.findUnique({
+            where: {
+                clerkId: clerk.id,
+            },
+            select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+            },
+        })
     } catch (error) {
         console.error(error)
         return null
